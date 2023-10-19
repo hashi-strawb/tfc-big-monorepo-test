7de1bb9d5b3f35ea2d66b89fd47809735daedaae
Lucy Davinhart <lucy@lucymhdavies.com>
2023-10-19 18:36:55 +0100

    update TFC config

--- tfc/main.tf
+++ tfc/main.tf
@@ -13,7 +13,7 @@ provider "tfe" {
 
 
 resource "tfe_workspace" "ws" {
-  count = 10
+  count = 500
 
   auto_apply        = true
   name              = "many-workspace-test-${format("%04d", count.index)}"
@@ -22,7 +22,16 @@ resource "tfe_workspace" "ws" {
   trigger_prefixes  = ["/modules"]
   working_directory = "workspaces/"
   vcs_repo {
+    # built-in github app (no custom webhooks)
     github_app_installation_id = "ghain-3JcBMQW48TPfPn4f"
-    identifier                 = "hashi-strawb/tfc-big-monorepo-test"
+
+    # custom github provider
+    # oauth_token_id             = "ot-TZxcR8MRaTRvteFL"
+
+    identifier = "hashi-strawb/tfc-big-monorepo-test"
   }
+
+  # allow me to delete a workspace without destroying its resource
+  # which is safe in this case, as they're all random pets
+  force_delete = true
 }
