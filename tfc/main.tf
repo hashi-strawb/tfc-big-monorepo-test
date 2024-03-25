b958f97b072b98c5a72c36723f8259bc3fc5e740
Lucy Davinhart <lucy@lucymhdavies.com>
2024-03-25 16:09:03 +0000

    A TFC workspace which always runs

--- tfc/main.tf
+++ tfc/main.tf
@@ -42,3 +42,29 @@ resource "tfe_workspace" "ws" {
   # which is safe in this case, as they're all random pets
   force_delete = true
 }
+
+
+# Workaround Workspace
+resource "tfe_workspace" "workaround" {
+  auto_apply        = true
+  name              = "workaround"
+  project_id        = tfe_project.test.id
+  working_directory = "workaround"
+
+  # Always trigger a run
+  file_triggers_enabled = false
+
+  vcs_repo {
+    # built-in github app (no custom webhooks)
+    github_app_installation_id = "ghain-3JcBMQW48TPfPn4f"
+
+    # custom github provider
+    # oauth_token_id             = "ot-TZxcR8MRaTRvteFL"
+
+    identifier = "hashi-strawb/tfc-big-monorepo-test"
+  }
+
+  # allow me to delete a workspace without destroying its resource
+  # which is safe in this case, as they're all random pets
+  force_delete = true
+}
